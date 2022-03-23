480d538b8f6fe21b9cb58bd9bda52c4e94bce024
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-23 11:46:06 +0000

    remove comments/ tidy up

--- navigation/authStack.js
+++ navigation/authStack.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Welcome from "../screens/Welcome";
@@ -10,12 +9,10 @@ const Stack = createStackNavigator();
 
 export default function AuthStack() {
   return (
-    // <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Sign In" component={login} />
-        <Stack.Screen name="Sign Up" component={SignOut} />
-      </Stack.Navigator>
-    // </NavigationContainer>
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Sign In" component={login} />
+      <Stack.Screen name="Sign Up" component={SignOut} />
+    </Stack.Navigator>
   );
 }
