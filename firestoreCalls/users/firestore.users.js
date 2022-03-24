ceaa5cce3f3a04efa37042d9bfe99abf0f1d371b
Chloe <chloe_g@btinternet.com>
2022-03-24 11:21:43 +0000

    changed addNewUser function to replicate new firestore subcollection structure

--- firestoreCalls/users/firestore.users.js
+++ firestoreCalls/users/firestore.users.js
@@ -1,16 +1,45 @@
 import db from "../../config/firestore";
 import { doc, setDoc } from "firebase/firestore";
 
-function addNewUser(uid, email) {
+function addNewUser(uid) {
   async function addUserDoc(db) {
-    const data = {
-      email: `${email}`,
-      animals_caught: {},
+    const countsData = {
+      total_count: 0,
+      puffin: 0,
+      owl: 0,
+      robin: 0,
+      deer: 0,
+      squirrel: 0,
+      hedgehog: 0,
+      pheasant: 0,
+      butterfly: 0,
+      fox: 0,
+      frog: 0,
+    };
+    const photosData = {
+      puffin: [],
+      owl: [],
+      robin: [],
+      deer: [],
+      squirrel: [],
+      hedgehog: [],
+      pheasant: [],
+      butterfly: [],
+      fox: [],
+      frog: [],
     };
     // Add a new document in collection "users" with title uid
-    const res = await setDoc(doc(db, "users", `${uid}`), data);
+    await setDoc(
+      doc(db, "users-animals", `${uid}`, "animals", "counts"),
+      countsData
+    );
+    await setDoc(
+      doc(db, "users-animals", `${uid}`, "animals", "photos"),
+      photosData
+    );
   }
   addUserDoc(db);
 }
+
 
 export default addNewUser;
