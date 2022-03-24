deab2ef4c59497fbc2e6b00bc1c005c626475cf2
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-24 16:19:43 +0000

    amend badges styling

--- Components/BadgeCard.jsx
+++ Components/BadgeCard.jsx
@@ -34,7 +34,7 @@ export default function BadgeCard({ count }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: "row",
     alignContent: "center",
     alignItems: "center",
     justifyContent: "center",
