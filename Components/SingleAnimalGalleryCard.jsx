0fe1a521bd277d1b1ef83023f8be1726fb07db2a
RchlLou <rachelbellwood22@googlemail.com>
2022-03-25 10:49:20 -0700

    Gallery functionality complete - small bug in modal click

--- Components/SingleAnimalGalleryCard.jsx
+++ Components/SingleAnimalGalleryCard.jsx
@@ -1,37 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
-import getAnimalsByUserId from "../firestoreCalls/users/firestore.animalsByUser";
-import { Text, View } from "react-native";
+import { Text, View, Image, Modal, Pressable, StyleSheet } from "react-native";
 import { Card } from "react-native-elements";
 
-export default function SingleAnimalGalleryCard({ animal, currentUser }) {
-  const [animalGallery, setAnimalGallery] = useState([]);
-  // const { user } = useAuthentication();
-  // const auth = getAuth();
-  // let currentUser;
-  // onAuthStateChanged(auth, (user) => {
-  //   if (user) {
-  //     currentUser = user;
-  //     console.log(currentUser);
-  //   }
-  // });
-  console.log("in single page");
-  useEffect(() => {
-    if (currentUser) {
-      console.log("here is current user", currentUser);
-      getAnimalsByUserId(currentUser)
-        .then((data) => {
-          console.log("data from get req", data);
-          console.log("animal", animal);
-          return data.filter((animalItem) => animalItem.name === animal.name);
-        })
-        .then((filtered) => {
-          setAnimalGallery(filtered);
-          console.log("animal gallery", animalGallery);
-        });
+export default function SingleAnimalGalleryCard({ animalName, animalUrls }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const { user } = useAuthentication();
+  const auth = getAuth();
+  let currentUser;
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      currentUser = user;
     }
-  }, []);
+  });
 
   // useEffect(() => {
   //   getAnimalsByUserId(currentUser).then((data) => {
@@ -41,16 +24,73 @@ export default function SingleAnimalGalleryCard({ animal, currentUser }) {
 
   // console.log("animal gallery", animalGallery);
 
+  console.log(animalUrls);
+  console.log(animalName);
   return (
     <View>
       <Card>
-        {/* <Text>
-          {animal.name}: {animal.count}
-        </Text> */}
-        {animalGallery.map((animalPic) => {
-          return <Image source={animalPic}></Image>;
+        <Text>
+          {animalName}: {animalUrls.length}
+        </Text>
+
+        {animalUrls.map((animalPic, index) => {
+          return (
+            <View key={index}>
+              <Pressable
+                style={{ width: 150, height: 150 }}
+                onPress={() => {
+                  setModalVisible(true);
+                }}
+              >
+                <Image
+                  source={animalPic}
+                  style={{ width: 150, height: 150 }}
+                ></Image>
+              </Pressable>
+
+              <Modal
+                key={index}
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                  Alert.alert("Modal has been closed.");
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <View style={styles.modalView}>
+                  <Pressable
+                    onPress={() => {
+                      setModalVisible(false);
+                    }}
+                  >
+                    <Text>X</Text>
+                  </Pressable>
+                  <Image
+                    source={animalPic}
+                    style={{ width: 150, height: 150 }}
+                  ></Image>
+                </View>
+              </Modal>
+            </View>
+          );
         })}
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+});
--- screens/CameraGallery.jsx
+++ screens/CameraGallery.jsx
@@ -1,44 +1,71 @@
 import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useAuthentication } from "../utils/hooks/useAuthentication";
+
+import getAnimalCounts from "../firestoreCalls/animals/firestore.animalCounts";
 import getAnimalsByUserId from "../firestoreCalls/users/firestore.animalsByUser";
 import SingleAnimalGalleryCard from "../Components/SingleAnimalGalleryCard";
 
 const GalleryPage = () => {
   const [animals, setAnimals] = useState([]);
-  const { user } = useAuthentication();
+  const [animalGallery, setAnimalGallery] = useState([]);
   const auth = getAuth();
   let currentUser;
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
       currentUser = user;
-      console.log(currentUser);
     }
   });
 
   useEffect(() => {
-    // setAnimals([
-    //   { name: "frog", count: 2 },
-    //   { name: "robin", count: 1 },
-    //   { name: "deer", count: 2 },
-    // ]);
-    getAnimalsByUserId(currentUser).then((data) => {
-      console.log("function return", data);
+    if (currentUser) {
+      getAnimalsByUserId(currentUser).then((data) => {
+        console.log(data);
+        setAnimalGallery(data);
+      });
+
+      // .then((data) => {
+      //   Object.keys(data).forEach((animalType) => {
+      //     return animalType, data[animalType];
+      //   });
+      //   const dataArr = Object.entries(data);
+      //   //  console.log("data", dataArr);
+
+      //   return dataArr.filter((animalItem) => {
+      //     //    console.log("animalItem", animalItem);
+      //     animalItem[0] === animal[0];
+      //   });
+      // })
+      // .then((filtered) => {
+      //   setAnimalGallery(filtered);
+      //   //   console.log("animal gallery", animalGallery);
+      // });
+    }
+  }, []);
+
+  // [[fox: 1], [hedgehog: 1], [allCounts: 2]]
+
+  useEffect(() => {
+    getAnimalCounts(currentUser).then((data) => {
+      const allCounts = Object.entries(data).filter(
+        (arr) => "total_count" !== arr[0]
+      );
+
+      setAnimals(allCounts);
     });
   }, []);
 
-  console.log("animal list", animals);
+  console.log(animals);
+
   return (
     <View>
       {animals.map((animal) => {
         return (
           <SingleAnimalGalleryCard
-            key={animal.name}
-            animal={animal}
-            currentUser={currentUser}
+            key={animal[0]}
+            animalName={animal[0]}
+            animalUrls={animalGallery[animal[0]]}
           />
         );
       })}
