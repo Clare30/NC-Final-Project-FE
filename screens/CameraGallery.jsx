baeb38bcd3c24ac6c7222c3a3c5be71bc443770c
RchlLou <rachelbellwood22@googlemail.com>
2022-03-25 04:13:03 -0700

    Gallerycard setup

---
+++ screens/CameraGallery.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import SingleAnimalGalleryCard from "../Components/SingleAnimalGalleryCard";
+
+const GalleryPage = () => {
+  return (
+    <View>
+      <SingleAnimalGalleryCard />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default GalleryPage;
