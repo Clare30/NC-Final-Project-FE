5c3eb746716d9e351a93d146adf0931fb8450239
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-25 11:34:46 +0000

    upload working

--- firestoreCalls/users/firestore.postPhoto.js
+++ firestoreCalls/users/firestore.postPhoto.js
@@ -1,38 +1,14 @@
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-export default async function postPhoto(uid, photo) {
+export default async function postPhoto(uid, uri) {
   const storage = getStorage();
-  const storageRef = ref(storage, "photos/test2.jpg");
+  const response = await fetch(uri);
+  const blob = await response.blob();
+  const storageRef = ref(storage, `photos/${uid}/${Date.now()}.jpg`);
 
   // 'file' comes from the Blob or File API
-  await uploadBytes(storageRef, photo);
+  const res = await uploadBytes(storageRef, blob);
+  const downloadURL = await getDownloadURL(storageRef)
+  console.log(downloadURL);
+  return res;
 }
-
-
-// const uploadImage = async () => {
-//   setIsItemUploading(true);
-//   const blob = await new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onload = function () {
-//       resolve(xhr.response);
-//     };
-//     xhr.onerror = function (err) {
-//       console.log(err);
-//       reject(new TypeError(“Network request failed”));
-//     };
-//     xhr.responseType = “blob”;
-//     xhr.open(“GET”, phoneImageUri, true);
-//     xhr.send(null);
-//   });
-//   const reference = ref(storage, `images/${uuid.v4()}.jpeg`);
-//   try {
-//     const snapshot = await uploadBytes(reference, blob);
-//     blob.close();
-//     const uri = `gs://${snapshot.metadata.bucket}/${snapshot.metadata.fullPath}`;
-//     return uri;
-//   } catch (err) {
-//     setIsItemUploading(false);
-//     alert(“Image failed to upload!“);
-//     console.log(err);
-//   }
-// };
