8ff2282f5c5fdb1bdcd151508b9fd3bcae0d03ce
cono07 <amy_oconnor07@hotmail.co.uk>
2022-03-25 14:27:51 +0000

    add missing return

--- firestoreCalls/users/firestore.animalsByUser.js
+++ firestoreCalls/users/firestore.animalsByUser.js
@@ -3,7 +3,7 @@ import { doc, getDoc } from "firebase/firestore";
 
 const getAnimalsByUserId = (user) => {
   const docRef = doc(db, "users-animals", `${user.uid}`, "animals", "photos");
-  getDoc(docRef).then((res) => {
+  return getDoc(docRef).then((res) => {
     return res.data();
   });
 };
