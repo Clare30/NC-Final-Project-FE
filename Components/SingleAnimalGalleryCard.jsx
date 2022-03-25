649174059e27360c82624f4e0013ca8f03cde304
cono07 <amy_oconnor07@hotmail.co.uk>
2022-03-25 14:34:48 +0000

    start of gallery page

--- Components/SingleAnimalGalleryCard.jsx
+++ Components/SingleAnimalGalleryCard.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthentication } from "../utils/hooks/useAuthentication";
+import getAnimalsByUserId from "../firestoreCalls/users/firestore.animalsByUser";
 import { Text, View } from "react-native";
+import { Card } from "react-native-elements";
 
-export default function SingleAnimalGalleryCard() {
+export default function SingleAnimalGalleryCard({ animal, currentUser }) {
+  const [animalGallery, setAnimalGallery] = useState([]);
+  // const { user } = useAuthentication();
+  // const auth = getAuth();
+  // let currentUser;
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     currentUser = user;
+  //     console.log(currentUser);
+  //   }
+  // });
+  console.log("in single page");
+  useEffect(() => {
+    if (currentUser) {
+      console.log("here is current user", currentUser);
+      getAnimalsByUserId(currentUser)
+        .then((data) => {
+          console.log("data from get req", data);
+          console.log("animal", animal);
+          return data.filter((animalItem) => animalItem.name === animal.name);
+        })
+        .then((filtered) => {
+          setAnimalGallery(filtered);
+          console.log("animal gallery", animalGallery);
+        });
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   getAnimalsByUserId(currentUser).then((data) => {
+  //     console.log("here in data", data);
+  //   });
+  // }, []);
+
+  // console.log("animal gallery", animalGallery);
+
   return (
     <View>
-      <Text>Hello</Text>
+      <Card>
+        {/* <Text>
+          {animal.name}: {animal.count}
+        </Text> */}
+        {animalGallery.map((animalPic) => {
+          return <Image source={animalPic}></Image>;
+        })}
+      </Card>
     </View>
   );
 }
--- screens/CameraGallery.jsx
+++ screens/CameraGallery.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { Card } from "react-native-elements";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthentication } from "../utils/hooks/useAuthentication";
+import getAnimalsByUserId from "../firestoreCalls/users/firestore.animalsByUser";
 import SingleAnimalGalleryCard from "../Components/SingleAnimalGalleryCard";
 
 const GalleryPage = () => {
+  const [animals, setAnimals] = useState([]);
+  const { user } = useAuthentication();
+  const auth = getAuth();
+  let currentUser;
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      currentUser = user;
+      console.log(currentUser);
+    }
+  });
+
+  useEffect(() => {
+    // setAnimals([
+    //   { name: "frog", count: 2 },
+    //   { name: "robin", count: 1 },
+    //   { name: "deer", count: 2 },
+    // ]);
+    getAnimalsByUserId(currentUser).then((data) => {
+      console.log("function return", data);
+    });
+  }, []);
+
+  console.log("animal list", animals);
   return (
     <View>
-      <SingleAnimalGalleryCard />
+      {animals.map((animal) => {
+        return (
+          <SingleAnimalGalleryCard
+            key={animal.name}
+            animal={animal}
+            currentUser={currentUser}
+          />
+        );
+      })}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+// const styles = StyleSheet.create({});
 export default GalleryPage;
