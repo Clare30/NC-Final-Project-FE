dec80ae0cbcd03f8a83a04c1a19ccc0963f90633
cono07 <amy_oconnor07@hotmail.co.uk>
2022-03-30 14:26:19 +0100

    add spinner to signIn page & change drawerNav header to white

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,18 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImageBackground, StyleSheet, View } from "react-native";
 
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-import {
-  Box,
-  Center,
-  FormControl,
-  Heading,
-  VStack,
-  Button,
-  Input,
-  Text,
-} from "native-base";
+import { Box, Center, FormControl, Heading, VStack, Button, Input, Text, HStack, Spinner } from "native-base";
 import signInBackground from "../graphics/scenes/sign-in-up-backdrop.png";
 import signInBox from "../graphics/scenes/sign-in-box.png";
 
@@ -25,7 +16,10 @@ const LoginScreen = ({ navigation }) => {
     error: "",
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+
   async function login() {
+    setIsLoading(true);
     if (value.email === "" || value.password === "") {
       setValue({
         ...value,
@@ -36,6 +30,7 @@ const LoginScreen = ({ navigation }) => {
 
     try {
       await signInWithEmailAndPassword(auth, value.email, value.password);
+      setIsLoading(false);
     } catch (error) {
       setValue({
         ...value,
@@ -45,92 +40,86 @@ const LoginScreen = ({ navigation }) => {
   }
 
   return (
-    <ImageBackground
-      resizeMode="cover"
-      style={{ flex: 1 }}
-      source={signInBackground}
-    >
-      <Center w="100%">
-        <Box safeArea p="2" py="8" w="90%" maxW="290">
-          <Heading
-            fontWeight={400}
-            size="lg"
-            color="#fff"
-            _dark={{
-              color: "warmGray.50",
-            }}
-          >
-            Welcome
-          </Heading>
-
-          {!!value.error && (
-            <View style={styles.error}>
-              <Text>{value.error}</Text>
-            </View>
-          )}
-
-          <VStack space={3} mt="20" style={styles.vstack}>
-            <Center w="100%" h="270">
-              <ImageBackground
-                source={signInBox}
-                style={styles.owlBox}
-                alt="BackImageBackground"
-              >
-                <Center w="50%">
-                  <FormControl>
-                    <FormControl.Label>Email</FormControl.Label>
-                    <Input
-                      w="100%"
-                      value={value.email}
-                      onChangeText={(text) =>
-                        setValue({ ...value, email: text })
-                      }
-                    />
-                  </FormControl>
-                  <FormControl>
-                    <FormControl.Label>Password</FormControl.Label>
-                    <Input
-                      secureTextEntry={true}
-                      w="100%"
-                      type="password"
-                      value={value.password}
-                      onChangeText={(text) =>
-                        setValue({ ...value, password: text })
-                      }
-                    />
-                  </FormControl>
-                  <Button mt="2" colorScheme="indigo" onPress={login}>
-                    Sign in
-                  </Button>
-                </Center>
-              </ImageBackground>
-            </Center>
-            <VStack mt="2" textAlign="center" alignContent="center">
-              <Text
-                fontSize="sm"
-                color="coolGray.600"
-                _dark={{
-                  color: "warmGray.200",
-                }}
-              >
-                I'm a new user
-              </Text>
-              <Text
-                onPress={() => {
-                  navigation.navigate("Sign Up");
-                }}
-                color="warmGray.200"
-                fontWeight="bold"
-                fontSize="sm"
-                textDecorationLine="underline"
-              >
-                Sign Up
-              </Text>
+    <>
+      {isLoading ? (
+        <ImageBackground resizeMode="cover" style={{ flex: 1 }} source={signInBackground}>
+          <Center w="100%" h="100%" style={{ flex: 1 }}>
+            <VStack space={2} justifyContent="center">
+              <Spinner accessibilityLabel="Loading posts" size="lg" color="#fff" />
+              <Heading color="#fff" fontSize="lg">
+                Loading
+              </Heading>
             </VStack>
-          </VStack>
-        </Box>
-      </Center>
-    </ImageBackground>
+          </Center>
+        </ImageBackground>
+      ) : (
+        <>
+          <ImageBackground resizeMode="cover" style={{ flex: 1 }} source={signInBackground}>
+            <Center w="100%">
+              <Box safeArea p="2" py="8" w="90%" maxW="290">
+                <Heading fontWeight={400} size="lg" color="#fff" _dark={{ color: "warmGray.50" }}>
+                  Welcome
+                </Heading>
+
+                {!!value.error && (
+                  <View style={styles.error}>
+                    <Text>{value.error}</Text>
+                  </View>
+                )}
+
+                <VStack space={3} mt="20" style={styles.vstack}>
+                  <Center w="100%" h="270">
+                    <ImageBackground source={signInBox} style={styles.owlBox} alt="BackImageBackground">
+                      <Center w="50%">
+                        <FormControl>
+                          <FormControl.Label>Email</FormControl.Label>
+                          <Input w="100%" value={value.email} onChangeText={(text) => setValue({ ...value, email: text })} />
+                        </FormControl>
+                        <FormControl>
+                          <FormControl.Label>Password</FormControl.Label>
+                          <Input
+                            secureTextEntry={true}
+                            w="100%"
+                            type="password"
+                            value={value.password}
+                            onChangeText={(text) => setValue({ ...value, password: text })}
+                          />
+                        </FormControl>
+                        <Button mt="2" colorScheme="indigo" onPress={login}>
+                          Sign in
+                        </Button>
+                      </Center>
+                    </ImageBackground>
+                  </Center>
+                  <VStack mt="2" textAlign="center" alignContent="center">
+                    <Text
+                      fontSize="sm"
+                      color="coolGray.600"
+                      _dark={{
+                        color: "warmGray.200",
+                      }}
+                    >
+                      I'm a new user
+                    </Text>
+                    <Text
+                      onPress={() => {
+                        navigation.navigate("Sign Up");
+                      }}
+                      color="warmGray.200"
+                      fontWeight="bold"
+                      fontSize="sm"
+                      textDecorationLine="underline"
+                    >
+                      Sign Up
+                    </Text>
+                  </VStack>
+                </VStack>
+              </Box>
+            </Center>
+          </ImageBackground>
+        </>
+      )}
+    </>
   );
 };
 
