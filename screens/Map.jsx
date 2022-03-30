8ba5776efb9e759340b5de6e0c17b131ecd06454
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-30 19:52:14 +0100

    add close button to map

--- screens/Map.jsx
+++ screens/Map.jsx
@@ -2,16 +2,8 @@ import React, { useState, useEffect } from "react";
 import MapView, { Marker, Callout } from "react-native-maps";
 import * as Location from "expo-location";
 import animalImages from "../graphics/animals";
-import {
-  ActivityIndicator,
-  Pressable,
-  StyleSheet,
-  Dimensions,
-  Modal,
-  Text,
-  View,
-  Image,
-} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { ActivityIndicator, Pressable, StyleSheet, Dimensions, Modal, Text, View, Image } from "react-native";
 
 export default function Map({ animals, showMap, setShowMap }) {
   const [location, setLocation] = useState({});
@@ -39,14 +31,7 @@ export default function Map({ animals, showMap, setShowMap }) {
       {!animals || !location.latitude ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <View>
-          <Pressable
-            onPress={() => {
-              setShowMap(false);
-            }}
-          >
-            <Text style={styles.text}>x close</Text>
-          </Pressable>
+        <View style={{ flex: 1 }}>
           <MapView maxZoom={12} initialRegion={location} style={styles.map}>
             {Object.keys(animals).map((animal) => {
               return animals[animal].map((animalData, index) => {
@@ -61,20 +46,19 @@ export default function Map({ animals, showMap, setShowMap }) {
                     }}
                   >
                     <Image style={styles.pin} source={animalImages[animal]} />
-                    <Callout style={styles.callOut}>
-                      <Text>
-                        <Image
-                          resizeMode="cover"
-                          style={styles.photo}
-                          source={{ uri: animalData.imageURL }}
-                        />
-                      </Text>
-                    </Callout>
                   </Marker>
                 );
               });
             })}
           </MapView>
+          <Pressable
+            style={styles.close}
+            onPress={() => {
+              setShowMap(false);
+            }}
+          >
+            <Ionicons name="close-circle-outline" size={50} color="#339999" />
+          </Pressable>
         </View>
       )}
     </Modal>
@@ -82,6 +66,7 @@ export default function Map({ animals, showMap, setShowMap }) {
 }
 const styles = StyleSheet.create({
   map: {
+    flex: 1,
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
   },
@@ -93,14 +78,11 @@ const styles = StyleSheet.create({
   },
   text: {
     textAlign: "center",
-    fontSize: 18,
   },
-  callOut: {
-    width: 250,
-    height: 250,
-  },
-  photo: {
-    width: 250,
-    height: 250,
+  close: {
+    position: "absolute",
+    flexDirection: "row",
+    alignSelf: "flex-end",
+    flex: 1,
   },
 });
