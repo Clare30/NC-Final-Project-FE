c391bd07f73f2e6b3c4414108973a303423a4494
ActuallySilver <52727508+ActuallySilver@users.noreply.github.com>
2022-04-06 18:24:17 +0100

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB28CNgXglmGuIQRtOOtK38eMKB55udqnE",
+  apiKey: "REDACTED",
   authDomain: "animon-c914f.firebaseapp.com",
   projectId: "animon-c914f",
   storageBucket: "animon-c914f.appspot.com",
   messagingSenderId: "42067647858",
-  appId: "1:42067647858:web:0423518a430196dc1991eb",
+  appId: "REDACTED",
 };
 
 // Initialize Firebase
